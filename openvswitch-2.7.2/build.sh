3d0ec35622d2b44f0a589bb1efccc1ebc1c5b3d4
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2017-09-01 17:20:52 +0200

    New tests for ovs 2.7.2

---
+++ openvswitch-2.7.2/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2016 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+. $(dirname $0)/../common.sh
+
+build_lib() {
+  rm -rf BUILD
+  cp -rf SRC BUILD
+  (cd BUILD && ./boot.sh && ./configure && make -j $JOBS)
+}
+
+get_git_tag https://github.com/openvswitch/ovs.git v2.7.2 SRC
+build_lib
+build_fuzzer
+set -x
+$CC $CFLAGS -c -g $SCRIPT_DIR/target.c -I BUILD -I BUILD/lib -I BUILD/include
+$CXX $CXXFLAGS target.o BUILD/lib/.libs/libopenvswitch.a -lz -lssl -lcrypto -latomic $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE
+$CC $CFLAGS -c -g $SCRIPT_DIR/target-ofp.c -I BUILD -I BUILD/lib -I BUILD/include
+$CXX $CXXFLAGS target-ofp.o BUILD/lib/.libs/libopenvswitch.a -lz -lssl -lcrypto -latomic $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE-ofp
+$CC $CFLAGS -c -g $SCRIPT_DIR/target-ct.c -I BUILD -I BUILD/lib -I BUILD/include
+$CXX $CXXFLAGS target-ct.o BUILD/lib/.libs/libopenvswitch.a -lz -lssl -lcrypto -latomic $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE-ct
